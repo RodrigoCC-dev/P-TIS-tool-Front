82e532184c50d7e97f28f2984c9188ea18f1f407
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-10-12 18:57:36 -0300

    Se agrega vista inicial de la aplicación

---
+++ src/views/Inicio.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+    <Header></Header>
+
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Inicio',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
