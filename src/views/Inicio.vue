4e38e44b153b88241cf9222d123fc6794404ced2
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-10-13 02:31:23 -0300

    Se agregan campos de email y password en la vista inical

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,7 +1,46 @@
 <template lang="html">
-  <div class="">
-    <Header></Header>
+  <div>
+    <Header/>
+    <div class="container">
+      <div id="init-container" class="columns">
+        <img src="@/assets/images/eao-usach.png" class="is-tablet" style="z-index: 1"/>
+        <div class="column is-3" style="z-index: 999"></div>
+        <div class="column is-6 bg-semitransparent px-5 py-5" style="z-index: 999">
+          <div class="title is-2">Módulo de minutas de reunión</div>
+          <div class="subtitle is-5 my-3">
+            <p>Herramienta de apoyo a las actividades de desarrollo de proyecto de software de los cursos P/TIS</p>
+          </div>
 
+          <div class="field">
+            <label class="label">Correo electrónico</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="nombre.apellido@usach.cl">
+            </div>
+          </div>
+          <br>
+          <div class="field">
+            <label class="label">Clave de acceso</label>
+            <div class="control">
+              <input class="input" type="password">
+            </div>
+          </div>
+
+          <div class="columns">
+            <div class="column is-3"></div>
+            <div class="column is-6">
+              <div class="field">
+                <div class="control">
+                  <button class="button is-link is-fullwidth">Acceder</button>
+                </div>
+              </div>
+            </div>
+            <div class="column is-3"></div>
+          </div>
+
+        </div>
+        <div class="column is-3" style="z-index: 999"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,4 +56,24 @@ export default {
 </script>
 
 <style lang="css" scoped>
+#init-container {
+  height: 650px;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.bg-semitransparent {
+  background-color: rgba(255, 255, 255, .8);
+}
+
+.has-textline-through {
+  text-decoration: line-through;
+}
+
+#init-container img {
+  position: absolute;
+  height: 650px;
+  object-fit: cover;
+}
 </style>
