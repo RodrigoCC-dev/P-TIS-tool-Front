477cd9131625a965ca6ad89ee6a4dc06948c0937
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-11-03 04:00:24 -0300

    Minutas: Se define la 'data' de la instancia 'vue' del formulario de creación de minuta

--- src/views/Minuta.vue
+++ src/views/Minuta.vue
@@ -3,6 +3,7 @@
     <Header/>
 
     <div class="container">
+
       <div class="columns">
         <div class="column is-full">
           <p class="title is-4">MINUTA DE REUNIÓN</p>
@@ -94,6 +95,7 @@
           </div>
         </div>
       </div>
+
       <div class="columns">
         <div class="column is-three-quarters is-offset-1 has-text-left">
           <div class="field">
@@ -119,25 +121,115 @@
       <div class="columns">
         <div class="column is-one-fifth">
           <p>Informativa</p>
-          <input type="checkbox" value="">
+          <div class="control">
+            <input type="checkbox" value="">
+          </div>
         </div>
         <div class="column">
           <p>Control de Avance</p>
-          <input type="checkbox" value="">
+          <div class="control">
+            <input type="checkbox" value="">
+          </div>
         </div>
         <div class="column">
           <p>Coordinador</p>
-          <input type="checkbox" value="">
+          <div class="control">
+            <input type="checkbox" value="">
+          </div>
         </div>
         <div class="column">
           <p>Decisión</p>
-          <input type="checkbox" value="">
+          <div class="control">
+            <input type="checkbox" value="">
+          </div>
         </div>
         <div class="column">
           <p>Otros</p>
-          <input type="checkbox" name="" value="">
+          <div class="control">
+            <input type="checkbox" name="" value="">
+          </div>
         </div>
       </div>
+
+      <div class="columns">
+        <div class="column is-full">
+          <p class="title is-5">Agenda / Objetivos</p>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-10 is-offset-1">
+          <div class="content has-text-left">
+            <dl>
+              <li>Primer objetivo</li>
+              <li>Segundo objetivo</li>
+              <li>Tercer objetivo</li>
+              <li>
+                <input class="input" type="text">
+              </li>
+            </dl>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column is-full">
+          <p class="title is-5">Resultados / Conclusiones</p>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-10 is-offset-1">
+          <div class="content has-text-left">
+            <dl>
+              <li>Primer resultado</li>
+              <li>Segundo resultado</li>
+              <li>
+                <input class="input" type="text">
+              </li>
+            </dl>
+          </div>
+        </div>
+      </div>
+
+      <div class="">
+        <br>
+        <table class="table is-hoverable is-fullwidth">
+          <thead>
+            <tr>
+              <th class="is-narrow">N°</th>
+              <th><abbr title="Tipo de actividad">Item</abbr></th>
+              <th><abbr title="Descripción de la actividad realizada">Descripción</abbr></th>
+              <th><abbr title="Fecha comprometida para la actividad">Fecha</abbr></th>
+              <th><abbr title="Responsable de realizarla">Responsable</abbr></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>INFO</td>
+              <td class="has-text-left">Se realizan el primer set de historias de usuario en Taiga.io</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th></th>
+              <td><select class="" name="">
+                <option value="">INFO</option>
+                <option value="">COMP</option>
+                <option value="">AGENDA</option>
+              </select></td>
+              <td><input class="input is-extend" type="text" name="" value=""></td>
+              <td><input class="input has-text-centered" type="date" name="" value=""></td>
+              <td><select class="" name="">
+                <option value="">-- SA --</option>
+                <option value="">MCR</option>
+                <option value="">PTR</option>
+                <option value="">ITZ</option>
+              </select></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
     </div>
 
     <Footer/>
@@ -153,6 +245,44 @@ export default {
   components: {
     Header,
     Footer
+  },
+  data () {
+    return {
+      minuta: {
+        codigo: '',
+        correlativo: 0,
+        fecha_reunion: '',
+        h_inicio: '',
+        h_termino: '',
+        clasificacion: {
+          informativa: false,
+          avance: false,
+          coordinacion: false,
+          decision: false,
+          otro: false
+        }
+      },
+      tema: '',
+      asistencia: {},
+      objetivos: [],
+      conclusiones: [],
+      item: {
+        correlativo: 0,
+        descripcion: '',
+        fecha: '',
+        tipo_item_id: 0
+      },
+      responsables: []
+      listaItems: [
+        {
+          correlativo: 0,
+          descripcion: '',
+          fecha: '',
+          tipo_item_id: 0
+        }
+      ],
+      tipo_items: []
+    }
   }
 }
 </script>
