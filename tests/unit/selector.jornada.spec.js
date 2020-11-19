0e1526e4314370e7ccb710f57f515a8e29e76f05
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-11-19 17:10:12 -0300

    Testing: Se agregan test unitarios inicialización de variables para componente 'SelectorJornada'

---
+++ tests/unit/selector.jornada.spec.js
@@ -0,0 +1,30 @@
+import { shallowMount } from '@vue/test-utils'
+import SelectorJornada from '@/components/SelectorJornada.vue'
+
+describe('SelectorJornada.vue', () => {
+  it('variable mostrarJornadas se inicializa correctamente', () => {
+    const wrapper = shallowMount(SelectorJornada)
+    expect(wrapper.vm.mostrarJornadas).toBeFalsy()
+  })
+
+  it('variable jornadaActual se inicializa correctamente', () => {
+    const wrapper = shallowMount(SelectorJornada)
+    expect(wrapper.vm.jornadaActual).toEqual('Diurna')
+  })
+
+  it('variable jornadasProfesor se inicializa correctamente', () => {
+    const wrapper = shallowMount(SelectorJornada)
+    expect(wrapper.vm.jornadasProfesor).toEqual([])
+  })
+
+  it('variable nombreTabs se inicializa correctamente', () => {
+    const esperado = {
+      diurna: 'Diurna',
+      vespertina: 'Vespertina'
+    }
+    const wrapper = shallowMount(SelectorJornada)
+    expect(wrapper.vm.nombreTabs).toEqual(esperado)
+  })
+
+  
+})
