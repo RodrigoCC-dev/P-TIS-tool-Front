cb1f4413105da2ba1d4c1171785c623163c194b4
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-15 05:57:36 -0300

    Testing: Se agrega test unitario método 'recibirComentarios' en 'comentar.minuta.spec.js'

--- tests/unit/comentar.minuta.spec.js
+++ tests/unit/comentar.minuta.spec.js
@@ -2,6 +2,32 @@ import { shallowMount } from '@vue/test-utils'
 import Comentarios from '@/components/comentarios/ComentarMinuta.vue'
 
 describe('ComentarMinuta.vue', () => {
+  const bitacora = {
+    id: 663462,
+    revision: 'Z',
+    minuta: {
+      id: 242345345,
+      correlativo: 6345,
+      tema: 'Esto es una prueba',
+      fecha_reunion: '2020-12-14T00:00:00.000Z',
+      h_inicio: '2020-12-14T23:00:00.000Z',
+      h_termino: '2020-12-14T23:59:00.000Z',
+      clasificacion: {
+        informativa: false,
+        avance: false,
+        decision: true,
+        coordinacion: false,
+        otro: false
+      },
+      asistencia: [
+        {
+          iniciales: 'GER',
+          descripcion: 'Presente'
+        }
+      ]
+    }
+  }
+
   it('se asigna props idBitacora adecuadamente', () => {
     const wrapper = shallowMount(Comentarios, {
       propsData: {
@@ -63,5 +89,20 @@ describe('ComentarMinuta.vue', () => {
       }
     })
     expect(wrapper.vm.aprobacion).toEqual(0)
+  })
+
+  it('método recibirComentarios funciona correctamente', () => {
+    const comentarios = [
+      {comentario: 'comentario de item', es_item: true, id_item: 6345},
+      {comentario: 'comentario general', es_item: false, id_item: 0}
+    ]
+    const wrapper = shallowMount(Comentarios, {
+      propsData: {
+        idBitacora: 1353
+      }
+    })
+    wrapper.vm.recibirComentarios(comentarios)
+    expect(wrapper.vm.comentarios).toEqual(comentarios)
+    expect(wrapper.vm.mostrarAprobacion).toBeTruthy()
   })
 })
