7c898d90a3a4b92382577e90001398fcf8a5f061
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-16 04:08:08 -0300

    comentarStake: Se limpia 'data' y 'methods' en vista 'Stakeholder'

--- src/views/Stakeholder.vue
+++ src/views/Stakeholder.vue
@@ -26,48 +26,9 @@ export default {
   },
   data () {
     return {
-      tabs: {
-        revision: 'is-active',
-        comentadas: '',
-        respondidas: '',
-        cerradas: ''
-      },
-      componentes: {
-        revision: 'inline',
-        comentadas: 'none',
-        respondidas: 'none',
-        cerradas: 'none'
-      },
-      mostrar: false
     }
   },
   methods: {
-    verRevision: function () {
-      return true
-    },
-    /*  verComentadas: function () {
-      for (var valor in this.tabs) {
-        if (valor === 'is-active') {
-          set(valor) = ''
-        }
-      }
-      this.tabs.comentadas = 'is-active'
-      for (var item in this.componentes) {
-        if (item === 'inline') {
-          this.componentes.set(item) = 'none'
-        }
-      }
-      this.componentes.comentadas = 'inline'
-      console.log(this.tabs)
-      console.log(this.componentes)
-      return true
-    }, */
-    verRespondidas: function () {
-      return true
-    },
-    verCerradas: function () {
-      return true
-    }
   }
 }
 </script>
