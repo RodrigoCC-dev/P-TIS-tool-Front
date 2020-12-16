0ca503e186942bcc47c1e1528271ecb89354dcf6
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-16 01:57:46 -0300

    Testing: Se agregan test unitarios a métodos 'validarActual' y 'validarNueva' en 'cambio.clave.spec.js'

--- tests/unit/cambio.clave.spec.js
+++ tests/unit/cambio.clave.spec.js
@@ -26,4 +26,70 @@ describe('CambioClave.vue', () => {
     const wrapper = shallowMount(CambioClave)
     expect(wrapper.vm.entradas).toEqual(esperado)
   })
+
+  it('método "validarActual" funciona correctamente para "actual" igual a "undefined"', () => {
+    const wrapper = shallowMount(CambioClave, {
+      data() {
+        return {
+          actual: undefined
+        }
+      }
+    })
+    expect(wrapper.vm.validarActual()).toBeFalsy()
+    expect(wrapper.vm.entradas.actual.error).toBeTruthy()
+    expect(wrapper.vm.entradas.actual.mensaje).toEqual('Este campo no puede estar vacío')
+  })
+
+  it('método "validarActual" funciona correctamente para "actual" igual a ""', () => {
+    const wrapper = shallowMount(CambioClave)
+    expect(wrapper.vm.validarActual()).toBeFalsy()
+    expect(wrapper.vm.entradas.actual.error).toBeTruthy()
+    expect(wrapper.vm.entradas.actual.mensaje).toEqual('No se ha ingresado la clave actual')
+  })
+
+  it('método "validarActual" funciona correctamente para "actual" igual a "abc"', () => {
+    const wrapper = shallowMount(CambioClave, {
+      data() {
+        return {
+          actual: 'abc'
+        }
+      }
+    })
+    expect(wrapper.vm.validarActual()).toBeTruthy()
+    expect(wrapper.vm.entradas.actual.error).toBeFalsy()
+    expect(wrapper.vm.entradas.actual.mensaje).toEqual('')
+  })
+
+  it('método "validarNueva" funciona correctamente para "nueva" igual a "undefined"', () => {
+    const wrapper = shallowMount(CambioClave, {
+      data() {
+        return {
+          nueva: undefined
+        }
+      }
+    })
+    expect(wrapper.vm.validarNueva()).toBeFalsy()
+    expect(wrapper.vm.entradas.nueva.error).toBeTruthy()
+    expect(wrapper.vm.entradas.nueva.mensaje).toEqual('Este campo no puede estar vacío')
+  })
+
+  it('método "validarNueva" funciona correctamente para "nueva" igual a ""', () => {
+    const wrapper = shallowMount(CambioClave)
+    expect(wrapper.vm.validarNueva()).toBeFalsy()
+    expect(wrapper.vm.entradas.nueva.error).toBeTruthy()
+    expect(wrapper.vm.entradas.nueva.mensaje).toEqual('No se ha ingresado la nueva clave')
+  })
+
+  it('método "validarNueva" funciona correctamente para "nueva" igual a "abc"', () => {
+    const wrapper = shallowMount(CambioClave, {
+      data() {
+        return {
+          nueva: 'abc'
+        }
+      }
+    })
+    expect(wrapper.vm.validarNueva()).toBeTruthy()
+    expect(wrapper.vm.entradas.nueva.error).toBeFalsy()
+    expect(wrapper.vm.entradas.nueva.mensaje).toEqual('')
+  })
 })
