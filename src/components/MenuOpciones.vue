8fbec5216e3879e579895316903e5ee3e8fe8974
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-15 21:40:56 -0300

    CambioClave: Se agregan funciones para eventos 'click' en menú desplegable del componente 'MenuOpciones'

--- src/components/MenuOpciones.vue
+++ src/components/MenuOpciones.vue
@@ -3,18 +3,18 @@
 
     <div class="dropdown is-hoverable">
       <div class="dropdown-trigger">
-        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu" @click="verMenu">
           <span>{{ this.nombreCompleto(this.usuario) }}</span>
           <span class="icon is-small">
             <i class="fas fa-angle-down" aria-hidden="true"></i>
           </span>
         </button>
       </div>
-      <div class="dropdown-menu" id="dropdown-menu" role="menu">
+      <div class="dropdown-menu" id="dropdown-menu" role="menu" v-if="mostrarMenu">
         <div class="dropdown-content">
           <a class="dropdown-item" @click="volverInicio">Inicio</a>
-          <a class="dropdown-item">Cambiar clave</a>
-          <a class="dropdown-item">Cerrar sesión</a>
+          <a class="dropdown-item" @click="cambioClave">Cambiar clave</a>
+          <a class="dropdown-item" @click="cerrarSesion">Cerrar sesión</a>
         </div>
       </div>
     </div>
@@ -23,17 +23,26 @@
 </template>
 
 <script>
+import Auth from '@/services/auth.js'
 import Funciones from '@/services/funciones.js'
 import { mapState } from 'vuex'
 
 export default {
   name: 'MenuOpciones',
+  data () {
+    return {
+      mostrarMenu: false
+    }
+  },
   computed: {
     ...mapState(['usuario'])
   },
   methods: {
     nombreCompleto: function (usuario) {
       return Funciones.nombreCompleto(usuario)
+    },
+    verMenu: function () {
+      this.mostrarMenu = !this.mostrarMenu
     },
     volverInicio: function () {
       if (this.usuario.rol.rango === 1) {
@@ -47,6 +56,16 @@ export default {
       } else {
         this.$router.push('/')
       }
+    },
+    cambiarClave: function () {
+      this.$router.push('usuario/cambio/clave')
+    },
+    cerrarSesion: function () {
+      localStorage.removeItem('user_tk')
+      this.$store.commit('setAutenticacion', false)
+      this.$store.commit('setUsuario', {})
+      Auth.deleteUser('userLogged')
+      return this.$router.push('/')
     }
   }
 }
