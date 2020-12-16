abfef25d777247a077880e125649dd842e6aab9c
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2020-12-15 21:33:11 -0300

    CambioClave: Se agregan validaciones a los campos de claves en el componente 'CambioClaves'

--- src/components/CambioClave.vue
+++ src/components/CambioClave.vue
@@ -52,6 +52,10 @@
 </template>
 
 <script>
+import axios from 'axios'
+import Auth from '@/services/auth.js'
+import { mapState } from 'vuex'
+
 export default {
   name: 'CambioClave',
   data () {
@@ -64,6 +68,10 @@ export default {
           error: false,
           mensaje: ''
         },
+        nueva: {
+          error: false,
+          mensaje: ''
+        },
         repetir: {
           error: false,
           mensaje: ''
@@ -71,27 +79,94 @@ export default {
       }
     }
   },
+  computed: {
+    ...mapState(['apiUrl', 'usuario'])
+  },
   methods: {
     validarActual: function () {
       if (this.actual !== undefined) {
-        return this.actual !== ''
+        if (this.actual !== '') {
+          this.entradas.actual.error = false
+          this.entradas.actual.mensaje = ''
+          return true
+        } else {
+          this.entradas.actual.error = true
+          this.entradas.actual.mensaje = 'No se ha ingresado la nueva clave'
+          return false
+        }
       } else {
+        this.entradas.actual.error = true
+        this.entradas.actual.mensaje = 'Este campo no puede estar vacío'
+        return false
+      }
+    },
+    validarNueva: function () {
+      if (this.nueva !== undefined) {
+        if (this.nueva !== '') {
+          this.entradas.nueva.error = false
+          this.entradas.nueva.mensaje = ''
+          return true
+        } else {
+          this.entradas.nueva.error = true
+          this.entradas.nueva.mensaje = 'No se ha ingresado la nueva clave'
+          return false
+        }
+      } else {
+        this.entradas.nueva.error = true
+        this.entradas.nueva.mensaje = 'Este campo no puede estar vacío'
         return false
       }
     },
     validarPass: function () {
       if (this.nueva !== undefined) {
         if (this.repetirNueva !== undefined) {
-          if (this.nueva !== '' && this.repetirNueva !== '') {
-            return this.nueva === this.repetirNueva
+          if (this.nueva !== '') {
+            if (this.repetirNueva !== '') {
+              if (this.nueva === this.repetirNueva) {
+                this.entradas.nueva.error = false
+                this.entradas.repetir.error = false
+                return true
+              } else {
+                this.entradas.nueva.error = true
+                this.entradas.nueva.mensaje = ''
+                this.entradas.repetir.error = true
+                this.entradas.repetir.mensaje = 'Las contraseñas no coinciden'
+                return false
+              }
+            } else {
+              this.entradas.repetir.error = true
+              this.entradas.repetir.mensaje = 'No se ha reingresado la nueva contraseña'
+              return false
+            }
           } else {
+            this.entradas.nueva.error = true
+            this.entradas.nueva.mensjae = 'No se ha ingresado la nueva contraseña'
             return false
           }
         } else {
+          this.entradas.repetir.error = true
+          this.entradas.repetir.mensaje = 'Falta reingresar la nueva contraseña'
           return false
         }
       } else {
+        this.entradas.nueva.error = true
+        this.entradas.nueva.mensaje = 'Falta ingresar la nueva contraseña'
         return false
+      }
+    }
+    async cambiarClave () {
+      const usuario = {
+        id: this.usuario.id,
+        password: this.actual
+        usuario: {
+          password: this.nueva,
+          password_confirmation: this.repetirNueva
+        }
+      }
+      try {
+        await axios.patch(this.apiUrl + '/usuarios/' + this.usuario.id, usuario, { headers: Auth.postHeader() })
+      } catch (e) {
+        console.log(e)
       }
     }
   }
