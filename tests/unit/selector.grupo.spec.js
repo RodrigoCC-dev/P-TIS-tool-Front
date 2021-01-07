65aa14898772488220c0aa645f5118f0d018a865
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-07 18:12:38 -0300

    Testing: Se agregan test unitarios de inicialización de variables para componente 'SelectorGrupo'

--- tests/unit/selector.grupo.spec.js
+++ tests/unit/selector.grupo.spec.js
@@ -2,5 +2,20 @@ import { shallowMount } from '@vue/test-utils'
 import SelectorGrupo from '@/components/SelectorGrupo.vue'
 
 describe('SelectorGrupo.vue', () => {
+  it('variable "grupoActual" se inicializa correctamente', () => {
+    const wrapper = shallowMount(SelectorGrupo)
+    expect(wrapper.vm.grupoActual).toEqual(0)
+  })
+
+  it('variable "grupoSeleccionado" se inicializa correctamente', () => {
+    const wrapper = shallowMount(SelectorGrupo)
+    expect(wrapper.vm.grupoSeleccionado).toEqual({})
+  })
+
+  it('variable "listaGrupos" se inicializa correctamente', () => {
+    const wrapper = shallowMount(SelectorGrupo)
+    expect(wrapper.vm.listaGrupos).toEqual([])
+  })
+
   
 })
