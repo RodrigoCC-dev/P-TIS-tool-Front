b5053e202035ef3603780adc13ecb450e934b07e
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-07 21:27:48 -0300

    Testing: Se agregan test unitarios a métodos del componente 'AsignarStk'

--- tests/unit/asignar.stakeholders.spec.js
+++ tests/unit/asignar.stakeholders.spec.js
@@ -12,6 +12,10 @@ describe('AsignarStk.vue', () => {
       email: 'daniel.castro@algo.com'
     }
   ]
+  const grupo = {
+    id:2, nombre: 'G02',
+    stakeholders: [{id: 962345}, {id: 7463945}, {id: 62345}]
+  }
 
   it('variable "grupoSeleccionado" se inicializa correctamente', () => {
     const wrapper = shallowMount(AsignarStk)
@@ -54,4 +58,46 @@ describe('AsignarStk.vue', () => {
     const wrapper = shallowMount(AsignarStk)
     expect(wrapper.vm.mostrarAsignar).toBeFalsy()
   })
+
+  it('método "seleccionarGrupo" funciona correctamente', () => {
+    const wrapper = shallowMount(AsignarStk)
+    wrapper.vm.seleccionarGrupo(grupo)
+    expect(wrapper.vm.grupoSeleccionado).toEqual(grupo)
+    expect(wrapper.vm.mostrarStakeholders).toBeTruthy()
+  })
+
+  it('método "nombreCompleto" funciona correctamente', () => {
+    const cliente = {
+      nombre: 'Mateo',
+      apellido_paterno: 'Iglesias',
+      apellido_materno: 'Del Campo'
+    }
+    const wrapper = shallowMount(AsignarStk)
+    expect(wrapper.vm.nombreCompleto(cliente)).toEqual('Mateo Iglesias Del Campo')
+  })
+
+  it('método "buscarIndex" funciona correctamente', () => {
+    const lista = [
+      {id: 29353}, {id: 9534}, {id: 926364}
+    ]
+    const wrapper = shallowMount(AsignarStk)
+    expect(wrapper.vm.buscarIndex(lista, 9534)).toEqual(1)
+  })
+
+  it('método "stakeholdersAsignados" funciona correctamente', () => {
+    const wrapper = shallowMount(AsignarStk)
+    wrapper.vm.grupoSeleccionado = grupo
+    wrapper.vm.listaStakeholders = listaStakeholders
+    wrapper.vm.stakeholdersAsignados()
+    expect(wrapper.vm.asignados[0]).toEqual(62345)
+  })
+
+  it('método "cerrar" funciona correctamente', async () => {
+    const wrapper = shallowMount(AsignarStk)
+    wrapper.vm.cerrar()
+    await wrapper.vm.$nextTick()
+    expect(wrapper.emitted().cerrar).toBeTruthy()
+    expect(wrapper.emitted().cerrar.length).toEqual(1)
+    expect(wrapper.emitted().cerrar[0]).toEqual([])
+  }) 
 })
