22db76a2a7510fba45839576f06b0efca1134317
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-12 03:23:03 -0300

    RevisionAv: Se agrega nueva pestaña de selección para revisar avances semanales en vista 'Coordinador'

---
+++ src/components/RevisionAvances.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <br>
+    <SelectorJornada/>
+
+    <SelectorGrupo @eleccion="seleccionarGrupo"/>
+
+  </div>
+</template>
+
+<script>
+import SelectorJornada from '@/components/SelectorJornada.vue'
+import SelectorGrupo from '@/components/SelectorGrupo.vue'
+
+export default {
+  name: 'RevisionAvances',
+  components: {
+    SelectorJornada,
+    SelectorGrupo
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
