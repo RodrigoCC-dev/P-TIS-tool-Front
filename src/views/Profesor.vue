fdcd57fc1b758fd0dba50998988f56e6e931100d
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-13 02:13:15 -0300

    Ordenamiento: Se cambia a componentes las vistas principales y se generan vistas simples que usan los componentes generados

--- src/views/Profesor.vue
+++ src/views/Profesor.vue
@@ -1,69 +1,28 @@
 <template>
   <div>
+
     <Header/>
 
     <div class="container">
-      <nav>
-        <div class="tabs">
-          <ul>
-            <li v-bind:class="{ 'is-active-usach' : nombreTabActiva === nombreTabs.Estudiantes }" v-on:click="elegirTab(nombreTabs.Estudiantes)"><a><strong>Estudiantes</strong></a></li>
-            <li v-bind:class="{ 'is-active-usach' : nombreTabActiva === nombreTabs.Grupos }" v-on:click="elegirTab(nombreTabs.Grupos)"><a><strong>Grupos</strong></a></li>
-            <li v-bind:class="{ 'is-active-usach' : nombreTabActiva === nombreTabs.Stakeholders}" v-on:click="elegirTab(nombreTabs.Stakeholders)"><a><strong>Clientes</strong></a></li>
-            <li v-bind:class="{ 'is-active-usach' : nombreTabActiva === nombreTabs.Minutas}" v-on:click="elegirTab(nombreTabs.Minutas)"><a><strong>Revisar Minutas</strong></a></li>
-            <li v-bind:class="{ 'is-active-usach' : nombreTabActiva === nombreTabs.Avances}" v-on:click="elegirTab(nombreTabs.Avances)"><a><strong>Revisar Avances</strong></a></li>
-          </ul>
-        </div>
-      </nav>
-      <GestionEstudiantes v-if="nombreTabActiva === nombreTabs.Estudiantes"/>
-      <GestionGrupos v-else-if="nombreTabActiva === nombreTabs.Grupos"/>
-      <RevisionMinutas v-else-if="nombreTabActiva === nombreTabs.Minutas"/>
-      <GestionClientes v-else-if="nombreTabActiva === nombreTabs.Stakeholders"/>
-      <RevisionAvances v-else-if="nombreTabActiva === nombreTabs.Avances"/>
-      <br>
+      <Profesores/>
     </div>
 
     <Footer/>
+
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
-import GestionEstudiantes from '@/components/GestionEstudiantes.vue'
-import GestionGrupos from '@/components/GestionGrupos.vue'
-import RevisionMinutas from '@/components/RevisionMinutas.vue'
-import GestionClientes from '@/components/GestionClientes.vue'
-import RevisionAvances from '@/components/RevisionAvances.vue'
-
-const nombreTabs = {
-  Estudiantes: 'estudiantes',
-  Grupos: 'grupos',
-  Minutas: 'minutas',
-  Stakeholders: 'clientes',
-  Avances: 'avances'
-}
+import Profesores from '@/components/views/Profesor.vue'
 
 export default {
-  name: 'Profesor',
+  name: 'Profesor.vue',
   components: {
     Header,
     Footer,
-    GestionEstudiantes,
-    GestionGrupos,
-    RevisionMinutas,
-    GestionClientes,
-    RevisionAvances
-  },
-  data () {
-    return {
-      nombreTabActiva: 'estudiantes',
-      nombreTabs
-    }
-  },
-  methods: {
-    elegirTab: function (nombreTab) {
-      this.nombreTabActiva = nombreTab
-    }
+    Profesores
   }
 }
 </script>
