93ad9d0831aa918f985455b2d3a34be039e41294
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-01-14 01:46:56 -0300

    Testing: Se incorpora inicialización de 'wrapper' con 'store' en test unitarios de componente 'RespuestasMinuta'

--- tests/unit/respuestas.minuta.spec.js
+++ tests/unit/respuestas.minuta.spec.js
@@ -1,16 +1,56 @@
 import { shallowMount } from '@vue/test-utils'
+import { createStore } from 'vuex'
 import Respuestas from '@/components/comentarios/RespuestasMinuta.vue'
 
+const store = createStore({
+  state() {
+    return {
+      apiUrl: '127.0.0.1:3000',
+      grupo: {
+        id: 96435,
+        nombre: 'G01',
+        proyecto: 'Proyecto de prueba',
+        correlativo: 9,
+        estudiantes: [
+          {id: 454, iniciales: 'ABC', usuario: {
+            nombre: 'Alberto',
+            apellido_paterno: 'Becerra',
+            apellido_materno: 'Cabrera',
+            run: '11111111-1',
+            email: 'alberto.becerra@algo.com'
+          }
+        }
+        ],
+        stakeholders: []
+      },
+      tipoAprobaciones: [
+        {id: 4353, identificador: 'A', descripcion: 'Aprobada', rango: 1},
+        {id: 94543, identificador: 'AC', descripcion: 'Aprobada con comentarios', rango: 2},
+        {id: 9454, identificador: 'R', descripcion: 'Rechazada', rango: 3},
+        {id: 235, identificador: 'RC', descripcion: 'Rechazada con comentarios', rango: 4}
+      ]
+    }
+  }
+})
+
 describe('RespuestasMinuta.vue', () => {
-  it('Debería ser true', () => {
-    expect(true).toBeTruthy()
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(Respuestas, {
+      global: {
+        plugins: [store]
+      }
+    })
   })
-  /*  Montaje depende de 'state'
 
   it('se asigna prop "idBitacora" correctamente', () => {
     const wrapper = shallowMount(Respuestas, {
       propsData: {
         idBitacora: 5
+      },
+      global: {
+        plugins: [store]
       }
     })
     expect(wrapper.props().idBitacora).toEqual(5)
@@ -20,6 +60,9 @@ describe('RespuestasMinuta.vue', () => {
     const wrapper = shallowMount(Respuestas, {
       propsData: {
         idBitacora: 5
+      },
+      global: {
+        plugins: [store]
       }
     })
     expect(wrapper.vm.id).toEqual(5)
@@ -29,6 +72,9 @@ describe('RespuestasMinuta.vue', () => {
     const wrapper = shallowMount(Respuestas, {
       propsData: {
         idBitacora: 5
+      },
+      global: {
+        plugins: [store]
       }
     })
     expect(wrapper.vm.bitacora).toEqual({})
@@ -38,6 +84,9 @@ describe('RespuestasMinuta.vue', () => {
     const wrapper = shallowMount(Respuestas, {
       propsData: {
         idBitacora: 5
+      },
+      global: {
+        plugins: [store]
       }
     })
     expect(wrapper.vm.comentarios).toEqual([])
@@ -47,10 +96,11 @@ describe('RespuestasMinuta.vue', () => {
     const wrapper = shallowMount(Respuestas, {
       propsData: {
         idBitacora: 5
+      },
+      global: {
+        plugins: [store]
       }
     })
     expect(wrapper.vm.aprobacion).toEqual(0)
   })
-
-  */
 })
