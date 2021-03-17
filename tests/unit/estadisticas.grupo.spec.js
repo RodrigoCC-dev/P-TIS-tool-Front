30dbb513a5180b888ac282dd3ff717b8b22be6eb
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-03-17 02:47:40 -0300

    Testing: Se agrega test unitarios para métodos asíncronos en componente 'EstadisticasGrupo'

--- tests/unit/estadisticas.grupo.spec.js
+++ tests/unit/estadisticas.grupo.spec.js
@@ -1,57 +1,84 @@
 import { shallowMount } from '@vue/test-utils'
+import { createStore } from 'vuex'
+import axios from 'axios'
 import EstadisticasGrupo from '@/components/EstadisticasGrupo.vue'
+
+// Creación del store
+const store = createStore({
+  state() {
+    return {
+      apiUrl: '127.0.0.1:3000'
+    }
+  }
+})
+
+// Variables
+const grupo = {
+  id: 945,
+  nombre: 'G01',
+  proyecto: 'Pruebas unitarias',
+  correlativo: 1,
+  jornada: 'Diurna',
+  estudiantes: [
+    {
+      id: 4634,
+      iniciales: 'ABC',
+      usuario: {
+        nombre: 'Juan',
+        apellido_paterno: 'Castro',
+        apellido_materno: 'Gonzalez',
+        run: '12345678-9',
+        email: 'juan.gonzales@algo.com'
+      }
+    }
+  ]
+}
+
+const registros = [
+  {
+    id: 4634,
+    iniciales: 'ABC',
+    usuario: {
+      id: 9433,
+      nombre: 'Juan',
+      apellido_paterno: 'Castro',
+      apellido_materno: 'Gonzalez',
+      run: '12345678-9',
+      registros: {
+        minutas: 43,
+        tema: 2,
+        objetivos: 13,
+        conclusiones: 7,
+        items: 131,
+        comentarios: 0,
+        respuestas: 83
+      }
+    }
+  }
+]
+
+// Mock Axios
+jest.mock('axios')
+
+axios.get.mockImplementation((url) => {
+  switch (url) {
+    case '127.0.0.1:3000/registros/grupo/945':
+      return Promise.resolve({data: registros})
+    default:
+      return Promise.reject(new Error('not found'))
+  }
+})
 
 describe('EstadisticasGrupo.vue', () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = shallowMount(EstadisticasGrupo)
+    wrapper = shallowMount(EstadisticasGrupo, {
+      global: {
+        plugins: [store]
+      }
+    })
   })
-
-  // Variables
-  const grupo = {
-    id: 945,
-    nombre: 'G01',
-    proyecto: 'Pruebas unitarias',
-    correlativo: 1,
-    jornada: 'Diurna',
-    estudiantes: [
-      {
-        id: 4634,
-        iniciales: 'ABC',
-        usuario: {
-          nombre: 'Juan',
-          apellido_paterno: 'Castro',
-          apellido_materno: 'Gonzalez',
-          run: '12345678-9',
-          email: 'juan.gonzales@algo.com'
-        }
-      }
-    ]
-  }
-
-  const registros = [
-    {
-      id: 4634,
-      iniciales: 'ABC',
-      usuario: {
-        id: 9433,
-        nombre: 'Juan',
-        apellido_paterno: 'Castro',
-        apellido_materno: 'Gonzalez',
-        run: '12345678-9',
-        registros: {
-          minutas: 43,
-          tema: 2,
-          objetivos: 13,
-          conclusiones: 7,
-          items: 131,
-          comentarios: 0,
-          respuestas: 83
-        }
-      }
-    }
-  ]
 
   // Comienzo de test unitarios
   it('variable "grupoSeleccionado" se inicializa correctamente', () => {
@@ -82,6 +109,19 @@ describe('EstadisticasGrupo.vue', () => {
 
   it('método "nombreCompleto" funciona correctamente', () => {
     expect(wrapper.vm.nombreCompleto(registros[0].usuario)).toEqual('Juan Castro Gonzalez')
+  })
+
+  it('método "seleccionGrupo" funciona correctamente', async () => {
+    wrapper.vm.seleccionGrupo(grupo)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.grupoSeleccionado).toEqual(grupo)
+    expect(wrapper.vm.registros).toEqual(registros)
+  })
+
+  it('método "obtenerRegistros" funciona correctamente', async () => {
+    wrapper.vm.obtenerRegistros(945)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.registros).toEqual(registros)
   })
 
   it('método "subTotalRegistros" funciona correctamente', () => {
