2320ac4bd702fe5fc624f8f9ab098c457d53fc84
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2021-03-17 02:31:59 -0300

    Testing: Se agregan test unitarios a métodos del componente 'EstadisticasGrupo'

--- tests/unit/estadisticas.grupo.spec.js
+++ tests/unit/estadisticas.grupo.spec.js
@@ -9,6 +9,27 @@ describe('EstadisticasGrupo.vue', () => {
   })
 
   // Variables
+  const grupo = {
+    id: 945,
+    nombre: 'G01',
+    proyecto: 'Pruebas unitarias',
+    correlativo: 1,
+    jornada: 'Diurna',
+    estudiantes: [
+      {
+        id: 4634,
+        iniciales: 'ABC',
+        usuario: {
+          nombre: 'Juan',
+          apellido_paterno: 'Castro',
+          apellido_materno: 'Gonzalez',
+          run: '12345678-9',
+          email: 'juan.gonzales@algo.com'
+        }
+      }
+    ]
+  }
+
   const registros = [
     {
       id: 4634,
@@ -57,5 +78,47 @@ describe('EstadisticasGrupo.vue', () => {
   it('propiedad computada "totalRegistros" funciona correctamente con "registros" distinto a inicial', () => {
     wrapper.vm.registros = registros
     expect(wrapper.vm.totalRegistros).toEqual(279)
+  })
+
+  it('método "nombreCompleto" funciona correctamente', () => {
+    expect(wrapper.vm.nombreCompleto(registros[0].usuario)).toEqual('Juan Castro Gonzalez')
+  })
+
+  it('método "subTotalRegistros" funciona correctamente', () => {
+    expect(wrapper.vm.subTotalRegistros(registros[0])).toEqual(279)
+  })
+
+  it('método "porcentajeParcial" funciona correctamente con valores mayores a cero', () => {
+    wrapper.vm.registros = registros
+    expect(wrapper.vm.porcentajeParcial(registros[0])).toEqual('100.0')
+  })
+
+  it('método "porcentajeParcial" funciona correctamente con "registros" vacío', () => {
+    expect(wrapper.vm.porcentajeParcial(registros[0])).toEqual(0)
+  })
+
+  it('método "porcentajeParcial" funciona correctamente con "registros" igual a cero', () => {
+    const estudiante = [{
+      id: 4634,
+      iniciales: 'ABC',
+      usuario: {
+        id: 9433,
+        nombre: 'Juan',
+        apellido_paterno: 'Castro',
+        apellido_materno: 'Gonzalez',
+        run: '12345678-9',
+        registros: {
+          minutas: 0,
+          tema: 0,
+          objetivos: 0,
+          conclusiones: 0,
+          items: 0,
+          comentarios: 0,
+          respuestas: 0
+        }
+      }
+    }]
+    wrapper.vm.registros = estudiante
+    expect(wrapper.vm.porcentajeParcial(estudiante[0])).toEqual('0.0')
   })
 })
