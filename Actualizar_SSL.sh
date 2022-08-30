c87eef721872847c112449a291899d566fe48257
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2022-08-29 23:33:25 -0400

    SSL: Se actualiza scripts de actualización

--- Actualizar_SSL.sh
+++ Actualizar_SSL.sh
@@ -6,19 +6,26 @@ docker rm ptis-front
 docker rmi ptisfront
 
 # Configurar la aplicación nueva
+echo .............................................................
 echo 'Ingrese dirección de la API, (ejemplo: https://tuapi.com):'
 read direccion
 echo "VUE_APP_API_URL='${direccion}'" > .env
 
 # Construir la aplicación
+echo .............................................................
+echo 'Se inicia construcción de la aplicación'
+echo .............................................................
 npm install
 npm run build
 cp Dockerfile Dockerfile.bk
 cp Dockerfile_SSL Dockerfile
 docker build -t ptisfront .
+echo .............................................................
+echo 'Aplicación construida exitósamente'
+echo .............................................................
 
 # Instalar la aplicación
 docker run --name ptis-front -p 80:80 -p 443:443 -d ptisfront
-
-# Limpiar instalación
-docker rmi registry.gitlab.com/rodrigo.castillo.c/p-tis-tool-front
+echo .............................................................
+echo 'Aplicación instalada correctamente'
+echo .............................................................
