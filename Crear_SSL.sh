43aba594a6c621889df046f31162041b6e7f7044
Rodrigo Castillo <rodrigo.castillo.c@usach.cl>
2023-03-30 02:20:12 -0300

    Actualización: Se actualizan scripts de instalación

--- Crear_SSL.sh
+++ Crear_SSL.sh
@@ -8,6 +8,7 @@ git clone https://github.com/acmesh-official/acme.sh.git
 cd acme.sh
 
 # instalar dependencias
+apt update
 apt install -y cron socat
 
 # Instalar acme.sh
@@ -22,5 +23,5 @@ read dominio
 
 # Copiar certificados creados en carpeta de acceso
 mkdir /certificates
-cp /root/.acme.sh/${dominio}/${dominio}.cer /certificates/cert_file.cer
-cp /root/.acme.sh/${dominio}/${dominio}.key /certificates/key_file.key
+cp /root/.acme.sh/${dominio}_ecc/${dominio}.cer /certificates/cert_file.cer
+cp /root/.acme.sh/${dominio}_ecc/${dominio}.key /certificates/key_file.key
